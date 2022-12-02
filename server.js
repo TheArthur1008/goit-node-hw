4aebbd496d98c08793bacbf37a3c90fdcb8501d8
Arthur Korostelev <korostelev.art.ua@gmail.com>
2022-12-02 21:04:23 +0200

    created hw-03

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const { PORT = 3000 } = process.env;
+const { CONNECT } = process.env;
+
+mongoose
+  .connect(CONNECT)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
